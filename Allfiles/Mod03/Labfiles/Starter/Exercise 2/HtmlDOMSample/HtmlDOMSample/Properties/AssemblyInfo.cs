9282fd4a750e12feaf2f3f4035892f35f595687b
Damian Hintz <damian.hintz@gmail.com>
2018-12-16 15:30:21 +0100

    Complete lesson 2 in module 3

---
+++ Allfiles/Mod03/Labfiles/Starter/Exercise 2/HtmlDOMSample/HtmlDOMSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Informacje ogólne o zestawie zależą od poniższego 
+// zestawu atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// związane z zestawem.
+[assembly: AssemblyTitle("HtmlDOMSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HtmlDOMSample")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie dla atrybutu ComVisible wartości false powoduje, że typy w tym zestawie stają się niewidoczne 
+// dla składników COM. Jeśli musisz uzyskiwać dostęp do typu w tym zestawie 
+// z modelu COM, ustaw dla atrybutu ComVisible tego typu wartość true.
+[assembly: ComVisible(false)]
+
+// Poniższy identyfikator GUID odpowiada atrybutowi ID biblioteki typów, jeśli ten projekt jest uwidaczniany w modelu COM
+[assembly: Guid("5c89f737-59e8-410b-bc21-2c1f0ec97746")]
+
+// Informacje o wersji zestawu obejmują następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza 
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie te wartości lub użyć wartości domyślnych numerów poprawki i kompilacji, 
+// stosując znak „*”, jak pokazano poniżej:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Allfiles/Mod03/Labfiles/Starter/Exercise 2/HtmlDOMSample/HtmlDOMSample/Scripts/IndexScript.js
@@ -0,0 +1,30 @@
+﻿function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+function addHobbies() {
+    var inputList = document.querySelectorAll('.hobbiesInput');
+    if (inputList.length < 5) {
+
+        var hobbiesList = document.getElementById('hobbiesList');
+        var newLineElement = createNode('br'),
+            inputElement = createNode('input');
+
+        inputElement.setAttribute("class", "hobbiesInput");
+
+        append(hobbiesList, newLineElement);
+        append(hobbiesList, inputElement);
+    }
+    else {
+        document.getElementById('newHobbyBtn').removeEventListener('click', addHobbies);
+    }
+}
+
+document.addEventListener("DOMContentLoaded",
+    function() {
+        document.getElementById('newHobbyBtn').addEventListener('click', addHobbies);
+    });
